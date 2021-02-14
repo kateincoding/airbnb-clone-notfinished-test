17f33740ac6d831271dd1beec471b7c21903bc81
Your Name <k.soto@pucp.pe>
2021-02-13 23:23:59 -0500

    first push

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import Home from './Home'
+import Header from './Header'
+
+function App() {
+  return (
+
+    //BEM
+    <div className="App">
+      <h1>airbnb-clone</h1>
+      <Header />
+      <Home />
+
+      {/* Home */}
+        {/* Header */}
+
+        {/* Banner */}
+          {/* Search */}
+ 
+        {/* Cards */}
+
+        {/* Footer */}
+
+      {/* SearchPage */}
+        {/*...*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+
+//ES7 snippets to do 'rfce'
+
+export default function Home() {
+    return (
+        <div className = 'home'>
+        {/*    <h1>Home component</h1>   */}
+        </div>
+    )
+}
